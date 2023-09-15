2fe9691dcd27926c1593159a84379f53afee4378
Guilherme Girardi <girardi.gui@icloud.com>
2023-09-15 11:23:36 +0100

    tidy-ups and style p. technologies page

--- src/pages/projectTechnologies/models/projectTechnologies.ts
+++ src/pages/projectTechnologies/models/projectTechnologies.ts
@@ -1,42 +1,62 @@
 export const projectTechnologies = [
   {
+    "id": 1,
     "title": "Material UI",
-    "description": ' for a polished and customizable UI design.'
+    "description": ' for a polished and customizable UI design.',
+    "url": "https://mui.com/",
   },
   {
+    "id": 2,
     "title": "JavaScript",
-    "description": ' for interactivity and functionality.'
+    "description": ' for interactivity and functionality.',
+    "url": "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
   },
   {
+    "id": 3,
     "title": "React",
-    "description": ' for building the user interface.'
+    "description": ' for building the user interface.',
+    "url": "https://react.dev/",
   },
   {
+    "id": 4,
     "title": "React Hooks",
-    "description": ' for managing state and side effects.'
+    "description": ' for managing state and side effects.',
+    "url": "https://legacy.reactjs.org/docs/hooks-intro.html",
   },
   {
+    "id": 5,
     "title": "React Error Boundary",
-    "description": ' for handling errors gracefully.'
+    "description": ' for handling errors gracefully.',
+    "url": "https://legacy.reactjs.org/docs/error-boundaries.html",
   },
   {
+    "id": 6,
     "title": "Typescript",
-    "description": ' for static typing and enhanced developer tooling.'
+    "description": ' for static typing and enhanced developer tooling.',
+    "url": "https://www.typescriptlang.org/",
   },
   {
+    "id": 7,
     "title": "JSON-server",
-    "description": ' for a mock API server.'
+    "description": ' for a mock API server.',
+    "url": "https://github.com/typicode/json-server",
   },
   {
+    "id": 8,
     "title": "Axios",
-    "description": ' for making HTTP requests.'
+    "description": ' for making HTTP requests.',
+    "url": "https://axios-http.com/",
   },
   {
+    "id": 9,
     "title": "React Testing Library",
-    "description": ' for testing React components.'
+    "description": ' for testing React components.',
+    "url": "https://testing-library.com/docs/react-testing-library/intro/",
   },
   {
+    "id": 10,
     "title": "MSW (Mock Service Worker)",
-    "description": ' for mocking HTTP requests in testing.'
+    "description": ' for mocking HTTP requests in testing.',
+    "url": "https://mswjs.io/",
   },
 ];
--- src/pages/projectTechnologies/projectTechnologies.tsx
+++ src/pages/projectTechnologies/projectTechnologies.tsx
@@ -1,14 +1,30 @@
-import { Grid, Toolbar, Typography } from '@mui/material';
+import { Box, Grid, List, ListItem, Toolbar, Typography, useTheme } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import AlakazamImg from 'assets/images/alakazam.png';
+import GyaradosImg from 'assets/images/gyarados.png';
+import SquirtleImg from 'assets/images/squirtle.jpeg';
 import { projectTechnologies } from './models/projectTechnologies';
 
 type TechnologyType = {
+  id: number;
   title: string;
   description: string;
+  url: string;
 };
 
 const ProjectTechnologies = () => {
+  const theme = useTheme();
+
   return (
-    <Grid>
+    <Grid
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        padding: theme.spacing(3),
+        width: '100%',
+        height: '100%'
+      }}
+    >
       <Toolbar
         sx={{
           minHeight: {
@@ -23,49 +39,141 @@ const ProjectTechnologies = () => {
         sx={{
           display: 'flex',
           flexDirection: 'column',
-          justifyContent: 'flex-start',
-          mt: 3,
-          padding: 0,
-          height: '100vh',
+          justifyContent: 'space-between',
+          width: '100%',
+          padding: theme.spacing(2),
+          border: '2px',
+          borderRadius: 3,
+          boxShadow: 'rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px',
         }}
       >
-        <Grid>
-          <Typography
+        <Grid sx={{ display: 'flex' }}>
+          <Grid
             sx={{
-              fontSize: 18,
-              textTransform: 'uppercase',
-              fontWeight: 600,
+              flex: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'flex-start',
+              margin: 0,
+              padding: 0,
             }}
           >
-            Project Technologies
-          </Typography>
+            <Grid>
+              <Typography
+                sx={{
+                  fontSize: 20,
+                  textTransform: 'uppercase',
+                  fontWeight: 600,
+                  marginBottom: 2,
+                }}
+              >
+                Project Technologies
+              </Typography>
+            </Grid>
+
+            <List>
+              {projectTechnologies.map((technology: TechnologyType) => (
+                <ListItem
+                  disablePadding
+                  key={technology.id}
+                  sx={{
+                    marginBottom: 1.5,
+                    fontSize: 14.5,
+                    fontWeight: 400,
+                  }}
+                >
+                  <NavLink
+                    to={technology.url}
+                    style={{
+                      fontWeight: 'bold',
+                      color: '#000',
+                      marginRight: '5px'
+                    }}
+                  >
+                    {technology.title}
+                  </NavLink>- {technology.description}
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+
+          <Grid
+            sx={{
+              flex: 2,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-around',
+            }}
+          >
+            <Grid
+              sx={{
+                display: 'flex',
+                alignSelf: 'flex-end',
+              }}
+            >
+              <Grid
+                sx={{
+                  height: 250,
+                  width: 210,
+                }}
+              >
+                <Box
+                  component="img"
+                  sx={{
+                    height: '100%',
+                    width: '100%',
+                  }}
+                  src={AlakazamImg}
+                  alt='Pokemon-logo'
+                />
+              </Grid>
+            </Grid>
+
+            <Grid
+              sx={{
+                display: 'flex',
+                alignSelf: 'flex-start',
+              }}
+            >
+              <Grid
+                sx={{
+                  height: 180,
+                  width: 170,
+                }}
+              >
+                <Box
+                  component="img"
+                  sx={{
+                    height: '100%',
+                    width: '100%',
+                    mixBlendMode: 'multiply'
+                  }}
+                  src={SquirtleImg}
+                  alt='Pokemon-logo'
+                />
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
 
-        <Grid>
-          <Typography
+        <Grid
+          sx={{
+            display: 'flex',
+            alignSelf: 'center',
+            height: 190,
+            width: 700,
+          }}
+        >
+          <Box
+            component="img"
             sx={{
-              marginTop: 2,
-              marginBottom: 2.5,
-              fontSize: 16,
-              fontWeight: 400,
+              height: '100%',
+              width: '100%',
             }}
-          >
-            I used several technologies to develop this website:
-          </Typography>
+            src={GyaradosImg}
+            alt='Pokemon-logo'
+          />
         </Grid>
-
-        {projectTechnologies.map((technology: TechnologyType, index: number) => (
-          <Typography
-            key={index}
-            sx={{
-              marginBottom: 1.5,
-              fontSize: 14.5,
-              fontWeight: 400,
-            }}
-          >
-            <strong>{technology.title}</strong> - {technology.description}
-          </Typography>
-        ))}
       </Grid>
     </Grid>
   );
