7cfef111421891942ab68abc9867a191ca0aea78
Guilherme Girardi <girardi.gui@icloud.com>
2023-10-17 14:19:36 -0300

    add projectTechnologies unit test

--- src/pages/navbar/projectTechnologies/tests/projectTechnologies.unit.test.tsx
+++ src/pages/navbar/projectTechnologies/tests/projectTechnologies.unit.test.tsx
@@ -2,11 +2,12 @@ import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import MockTheme from 'tests/mockComponents/mockTheme';
+import translations from 'utils/translations';
 import ProjectTechnologies from '../projectTechnologies';
 
 describe('Project Technologies Component', () => {
   const renderProjectTechnologies = () => {
-    return (
+    render(
       <BrowserRouter>
         <MockTheme>
           <ProjectTechnologies />
@@ -19,6 +20,21 @@ describe('Project Technologies Component', () => {
 
   it('should render the component', () => {
     expect(screen).not.toBeNull();
-    screen.debug(undefined, 9999999)
+  });
+
+  it('should render the title, links with description and image', async () => {
+    const title = await screen.findByText(translations.projectStatistics.title);
+    const materialUILink = await screen.findByRole('link', {
+      name: /material ui/i
+    });
+    const materialUIDescription = screen.getByText(/\- for a polished and customizable ui design\./i);
+
+    expect(title).toBeInTheDocument();
+    expect(materialUILink).toBeInTheDocument();
+    expect(materialUIDescription).toBeInTheDocument();
+  });
+
+  it('should match the snapshot', () => {
+    expect(screen).toMatchSnapshot();
   });
 });
