f749062b6fece4995ea8e74ce515f087bb29a3a7
Guilherme Girardi <girardi.gui@icloud.com>
2023-10-18 11:33:11 -0300

    refactor sections

---
+++ src/pages/menu/dashboard/components/section/section.tsx
@@ -0,0 +1,80 @@
+import { Grid, Typography } from '@mui/material';
+import clsx from 'clsx';
+import MuiSkeleton from 'components/skeleton/muiSkeleton';
+import { styles } from 'pages/menu/dashboard/css/sections.styles';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+const Section = ({
+  oddSection,
+  contentInnerWidth,
+  title,
+  subtitle,
+  subtitle2,
+  subtitle3,
+  image,
+  altImage,
+  skeletonWidth,
+  skeletonHeight,
+}: {
+  oddSection: boolean;
+  contentInnerWidth: any;
+  title: string;
+  subtitle: string;
+  subtitle2?: string;
+  subtitle3?: string;
+  image: string;
+  altImage: string;
+  skeletonWidth: number;
+  skeletonHeight: number;
+}) => {
+  const classes = styles(contentInnerWidth);
+
+  return (
+    <Grid
+      className={classes.container}
+      sx={{
+        backgroundColor: oddSection ? '#b3a1254d' : 'unset',
+        color: oddSection ? 'darkGrey.main' : 'inherit',
+      }}
+    >
+      <Grid className={classes.section}>
+        <Grid className={classes.evenTxtSection}>
+          <Typography className={classes.title}>
+            {title}
+          </Typography>
+
+          <Typography className={classes.text}>
+            {subtitle}
+          </Typography>
+
+          <Typography className={classes.text}>
+            {subtitle2}
+          </Typography>
+
+          <Typography className={classes.text}>
+            {subtitle3}
+          </Typography>
+        </Grid>
+
+        <Grid className={clsx(classes.imageContent, classes.evenImgSection)}>
+          <LazyLoadImage
+            src={image}
+            placeholder={
+              <MuiSkeleton
+                variant='rounded'
+                width={skeletonWidth}
+                height={skeletonHeight}
+              />
+            }
+            effect='blur'
+            width='100%'
+            height='auto'
+            alt={altImage}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Section;
--- src/pages/menu/dashboard/dashboard.tsx
+++ src/pages/menu/dashboard/dashboard.tsx
@@ -1,14 +1,16 @@
 import { Grid } from '@mui/material';
+import AerodactylImg from 'assets/images/aerodactyl.png';
+import CharmanderImg from 'assets/images/char.jpeg';
+import CharizardImg from 'assets/images/charizard.png';
+import GengarImg from 'assets/images/gengar.png';
+import MachampImg from 'assets/images/machamp.png';
 import { ErrorFallbackComponent } from 'components/errorFallbackComponent/errorFallbackComponent';
 import MuiButton from 'components/muiButton/muiButton';
 import MuiToolbar from 'components/muiToolbar/muiToolbar';
 import { useTranslations } from 'hooks/useTranslations';
 import { withErrorBoundary } from 'react-error-boundary';
-import FifthSection from './components/fifthSection/fifithSection';
-import FirstSection from './components/firstSection/firstSection';
-import SecondSection from './components/secondSection/secondSection';
-import SixthSection from './components/sixthSection/sixthSection';
-import ThirdSection from './components/thirdSection/thirdSection';
+import Section from './components/section/section';
+
 const Dashboard = withErrorBoundary(({
   contentInnerWidth,
   componentRef,
@@ -18,15 +20,68 @@ const Dashboard = withErrorBoundary(({
   componentRef: any;
   handleLogOut: () => void;
 }) => {
+  const translations = useTranslations();
+
   return (
     <Grid ref={componentRef}>
       <MuiToolbar />
 
-      <FirstSection contentInnerWidth={contentInnerWidth} />
-      <SecondSection contentInnerWidth={contentInnerWidth} />
-      <ThirdSection contentInnerWidth={contentInnerWidth} />
-      <FifthSection contentInnerWidth={contentInnerWidth} />
-      <SixthSection contentInnerWidth={contentInnerWidth} />
+      <Section
+        oddSection={false}
+        contentInnerWidth={contentInnerWidth}
+        title={translations.dashboard.worldOfPokemonTitle}
+        subtitle={translations.dashboard.worldOfPokemonTxt}
+        image={GengarImg}
+        altImage={'gengar-image-png'}
+        skeletonWidth={230}
+        skeletonHeight={230}
+      />
+
+      <Section
+        oddSection={true}
+        contentInnerWidth={contentInnerWidth}
+        title={translations.dashboard.exploreTitle}
+        subtitle={translations.dashboard.exploreTxt}
+        image={AerodactylImg}
+        altImage={'aerodactyl-image-png'}
+        skeletonWidth={180}
+        skeletonHeight={180}
+      />
+
+      <Section
+        oddSection={false}
+        contentInnerWidth={contentInnerWidth}
+        title={translations.dashboard.collectTitle}
+        subtitle={translations.dashboard.collectTxt}
+        image={MachampImg}
+        altImage={'machamp-image-png'}
+        skeletonWidth={180}
+        skeletonHeight={180}
+      />
+
+      <Section
+        oddSection={true}
+        contentInnerWidth={contentInnerWidth}
+        title={translations.dashboard.connectTitle}
+        subtitle={translations.dashboard.connectTxt}
+        image={CharizardImg}
+        altImage={'charizard-image-png'}
+        skeletonWidth={180}
+        skeletonHeight={180}
+      />
+
+      <Section
+        oddSection={false}
+        contentInnerWidth={contentInnerWidth}
+        title={translations.dashboard.discoverTitle}
+        subtitle={translations.dashboard.discoverTxt}
+        subtitle2={translations.dashboard.discoverTxt2}
+        subtitle3={translations.dashboard.discoverTxt3}
+        image={CharmanderImg}
+        altImage={'charmander-image-png'}
+        skeletonWidth={180}
+        skeletonHeight={180}
+      />
 
       {/* <MuiButton
         type='button'
